99fe29256fa37d217268d6058eafa5faef446e11
Jean Rodriguez <jeanrodriguez24@gmail.com>
2022-12-20 13:44:23 -0500

    Day 15

--- lib/advent_of_code/day_15.ex
+++ lib/advent_of_code/day_15.ex
@@ -24,33 +24,15 @@ defmodule AdventOfCode.Day15 do
 
   defp manhattan_distance(x1, x2, y1, y2), do: abs(x1 - x2) + abs(y1 - y2)
 
-  def offsets(0, _cnt, acc), do: acc
+  def closest_in_row(d, row, x1, y1) when row < y1 + d and row > y1 - d do
+    inset = abs(y1 - row)
 
-  def offsets(d, cnt, acc) do
-    offsets(
-      d - 1,
-      cnt + 1,
-      List.flatten(
-        for n <- -d..d, m <- -cnt..cnt do
-          [{n, m}, {m, n}]
-        end
-      )
-      |> MapSet.new()
-      |> MapSet.union(acc)
-    )
+    for x <- (-d + inset)..(d - inset), into: MapSet.new() do
+      {x1 + x, row}
+    end
   end
 
-  def closest(manhattan_distance, row, x1, y1) do
-    offsets(manhattan_distance, 0, MapSet.new())
-    |> Enum.reduce([], fn {x2, y2}, acc ->
-      if y1 + y2 == row do
-        [{x1 + x2, y1 + y2} | acc]
-      else
-        acc
-      end
-    end)
-    |> MapSet.new()
-  end
+  def closest_in_row(_, _, _, _), do: MapSet.new()
 
   defp no_beacons_helper([], _, s), do: s
 
@@ -60,7 +42,7 @@ defmodule AdventOfCode.Day15 do
     no_beacons_helper(
       rest,
       row,
-      MapSet.union(closest(d, row, x1, y1), s)
+      MapSet.union(closest_in_row(d, row, x1, y1), s)
     )
   end
 
@@ -68,26 +50,97 @@ defmodule AdventOfCode.Day15 do
     no_beacons_helper(pairs, row, MapSet.new())
   end
 
-  def part1(input) do
-    row = 2_000_000
+  defp beacons_helper([], _row, s), do: s
 
-    pairs = pairs(input)
+  defp beacons_helper([{_, {x2, y2}} | rest], row, s) do
+    new_s =
+      if y2 == row do
+        MapSet.put(s, {x2, y2})
+      else
+        s
+      end
 
-    found_beacons =
-      Enum.map(pairs, fn {{_, _}, {bx, by}} -> {bx, by} end)
-      |> Enum.filter(fn {_bx, by} -> by == row end)
-      |> MapSet.new()
-
-    pairs
-    |> Enum.filter(fn {{x1, y1}, {x2, y2}} ->
-      d = manhattan_distance(x1, x2, y1, y2)
-      row > y1 - d and row < y1 + d
-    end)
-    |> no_beacons(row)
-    |> MapSet.union(found_beacons)
-    |> Enum.count(fn {_x, y} -> y == row end)
+    beacons_helper(rest, row, new_s)
   end
 
-  def part2(_args) do
+  defp beacons(pairs, row) do
+    beacons_helper(pairs, row, MapSet.new())
+  end
+
+  def part1(input, row) do
+    pairs = pairs(input)
+
+    pairs
+    |> no_beacons(row)
+    |> IO.inspect()
+    |> MapSet.difference(beacons(pairs, row))
+    |> Enum.count()
+  end
+
+  def ranges_for_column([], _, _, acc), do: acc
+
+  def ranges_for_column([{_, sensor_y, d} | rest], search_space, row, acc)
+      when sensor_y + d < row or sensor_y - d > row,
+      do: ranges_for_column(rest, search_space, row, acc)
+
+  def ranges_for_column([{sensor_x, sensor_y, d} | rest], search_space, row, acc) do
+    inset = abs(sensor_y - row)
+    sensor_min = max(sensor_x - d + inset, 0)
+    sensor_max = min(sensor_x + d - inset, search_space)
+    ranges_for_column(rest, search_space, row, MapSet.put(acc, sensor_min..sensor_max))
+  end
+
+  def merge_ranges([r1 | []], []), do: [r1]
+  def merge_ranges([r2 | []], [r1 | []]), do: [r1, r2]
+
+  def merge_ranges(
+        [%Range{first: s1, last: e1} = r1, %Range{first: s2, last: e2} = r2 | rest],
+        acc
+      ) do
+    if e1 + 1 >= s2 do
+      new_range = s1..max(e1, e2)
+      merge_ranges([new_range | rest], acc)
+    else
+      merge_ranges([r2 | rest], [r1 | acc])
+    end
+  end
+
+  def beacon_position([_ | []], _), do: nil
+
+  def beacon_position([%Range{first: 0, last: e1}, %Range{last: e2} | []], search_space)
+      when e2 == search_space,
+      do: e1 + 1
+
+  def beacon_position([%Range{first: s1}, %Range{last: e2} | []], search_space)
+      when e2 == search_space,
+      do: s1 - 1
+
+  def beacon_position([%Range{first: 0}, %Range{last: l1} | []], _),
+    do: l1 + 1
+
+  @spec distress_beacons_helper([{any, number, number}], any, number) :: no_return
+  def distress_beacons_helper(sensors, search_space, row) do
+    ranges =
+      ranges_for_column(sensors, search_space, row, MapSet.new())
+      |> Enum.sort()
+      |> merge_ranges([])
+
+    x = beacon_position(ranges, search_space)
+    if x == nil, do: distress_beacons_helper(sensors, search_space, row + 1), else: {x, row}
+  end
+
+  def distress_beacon(pairs, search_space, row) do
+    pairs
+    |> Enum.map(fn {{x1, y1}, {x2, y2}} ->
+      d = manhattan_distance(x1, x2, y1, y2)
+      {x1, y1, d}
+    end)
+    |> distress_beacons_helper(search_space, row)
+  end
+
+  def part2(input, search_space) do
+    pairs(input)
+    |> distress_beacon(search_space, 0)
+    |> then(fn {x, y} -> x * 4_000_000 + y end)
   end
 end
--- test/advent_of_code/day_15_test.exs
+++ test/advent_of_code/day_15_test.exs
@@ -3,19 +3,6 @@ defmodule AdventOfCode.Day15Test do
 
   import AdventOfCode.Day15
 
-  test "closest" do
-    result = closest(3, 6, 8, 7)
-
-    assert result ==
-             MapSet.new([
-               {6, 6},
-               {7, 6},
-               {8, 6},
-               {9, 6},
-               {10, 6}
-             ])
-  end
-
   test "part1" do
     input = """
     Sensor at x=2, y=18: closest beacon is at x=-2, y=15
@@ -34,16 +21,31 @@ defmodule AdventOfCode.Day15Test do
     Sensor at x=20, y=1: closest beacon is at x=15, y=3
     """
 
-    result = part1(input)
+    result = part1(input, 10)
 
-    assert result == 27
+    assert result == 26
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
-    result = part2(input)
+    input = """
+    Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+    Sensor at x=9, y=16: closest beacon is at x=10, y=16
+    Sensor at x=13, y=2: closest beacon is at x=15, y=3
+    Sensor at x=12, y=14: closest beacon is at x=10, y=16
+    Sensor at x=10, y=20: closest beacon is at x=10, y=16
+    Sensor at x=14, y=17: closest beacon is at x=10, y=16
+    Sensor at x=8, y=7: closest beacon is at x=2, y=10
+    Sensor at x=2, y=0: closest beacon is at x=2, y=10
+    Sensor at x=0, y=11: closest beacon is at x=2, y=10
+    Sensor at x=20, y=14: closest beacon is at x=25, y=17
+    Sensor at x=17, y=20: closest beacon is at x=21, y=22
+    Sensor at x=16, y=7: closest beacon is at x=15, y=3
+    Sensor at x=14, y=3: closest beacon is at x=15, y=3
+    Sensor at x=20, y=1: closest beacon is at x=15, y=3
+    """
 
-    assert result
+    result = part2(input, 20)
+
+    assert result == 56_000_011
   end
 end
