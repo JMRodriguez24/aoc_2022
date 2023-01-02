6dad3142d13293cd062e0b839a31ffcc69dbe285
Jean Rodriguez <jeanrodriguez24@gmail.com>
2023-01-02 12:25:53 -0500

    Day 16

--- lib/advent_of_code/day_16.ex
+++ lib/advent_of_code/day_16.ex
@@ -1,5 +1,212 @@
 defmodule AdventOfCode.Day16 do
-  def part1(_args) do
+  defmodule Valve do
+    defstruct name: nil, flow_rate: 0, neighbors: nil
+
+    def new(name, flow_rate, neighbors) do
+      %Valve{
+        name: name,
+        flow_rate: flow_rate,
+        neighbors: neighbors
+      }
+    end
+  end
+
+  @line_regex ~r/Valve ([A-Z][A-Z]) has flow rate=(\d+); tunnel[s]? lead[s]? to valve[s]? (.+)/
+  def parse(input) do
+    String.trim(input)
+    |> String.split("\n")
+    |> Enum.map(fn line ->
+      [_match | [name, flow_rate_str, valves_str]] = Regex.run(@line_regex, line)
+      {flow_rate, _} = Integer.parse(flow_rate_str)
+      neighbors = String.split(valves_str, ",") |> Enum.map(&String.trim/1)
+      {name, Valve.new(name, flow_rate, neighbors)}
+    end)
+    |> Enum.into(%{})
+  end
+
+  defp should_open_valve?({_k, %{flow_rate: flow_rate}}) when flow_rate > 0, do: true
+  defp should_open_valve?(_), do: false
+
+  defp calculate_max_pressure(_, _, _, [], pressure_released), do: pressure_released
+
+  defp calculate_max_pressure(valves, open_valves, time_limit, [valve | rest], pressure_released) do
+    minute_opened = open_valves[valve]
+    minutes_opened = max(time_limit - minute_opened, 0)
+    pressure_released = pressure_released + valves[valve].flow_rate * minutes_opened
+    calculate_max_pressure(valves, open_valves, time_limit, rest, pressure_released)
+  end
+
+  defp shortest_path_length_helper(_, start_valve, end_valve, _, _, memo)
+       when start_valve == end_valve do
+    Agent.update(memo, &Map.put(&1, {start_valve, end_valve}, 0))
+  end
+
+  defp shortest_path_length_helper(_, start_valve, end_valve, [n | _] = path, _, memo)
+       when n == end_valve do
+    l = length(path) - 1
+    Agent.update(memo, &Map.put(&1, {start_valve, end_valve}, l))
+    l
+  end
+
+  defp shortest_path_length_helper(
+         valves,
+         start_valve,
+         end_valve,
+         [current | _] = path,
+         visited,
+         memo
+       ) do
+    cached_value = Agent.get(memo, &Map.get(&1, {start_valve, end_valve}))
+
+    cond do
+      cached_value != nil ->
+        cached_value
+
+      MapSet.member?(visited, current) ->
+        nil
+
+      true ->
+        neighbors = valves[current].neighbors
+
+        Enum.map(neighbors, fn n ->
+          shortest_path_length_helper(
+            valves,
+            start_valve,
+            end_valve,
+            [n | path],
+            MapSet.put(visited, current),
+            memo
+          )
+        end)
+        |> Enum.min()
+    end
+  end
+
+  def shortest_path_length(valves, current_valve, next_valve, memo) do
+    shortest_path_length_helper(
+      valves,
+      current_valve,
+      next_valve,
+      [current_valve],
+      MapSet.new(),
+      memo
+    )
+  end
+
+  defp open_valves_helper(
+         valves,
+         valves_to_open,
+         time_limit,
+         _,
+         path,
+         open_valves,
+         _
+       )
+       when length(valves_to_open) == length(path) - 1 do
+    IO.puts("all open")
+    IO.inspect(path)
+  end
+
+  defp open_valves_helper(valves, _, time_limit, minutes_elapsed, path, open_valves, _)
+       when minutes_elapsed >= time_limit do
+    p = calculate_max_pressure(valves, open_valves, time_limit, Map.keys(open_valves), 0)
+    IO.puts("#{inspect(path)} #{p}")
+    p
+  end
+
+  defp open_valves_helper(
+         valves,
+         valves_to_open,
+         time_limit,
+         minutes_elapsed,
+         [current_valve | _] = path,
+         open_valves,
+         memo
+       ) do
+    Enum.filter(valves_to_open, fn valve_to_open ->
+      not Map.has_key?(open_valves, valve_to_open)
+    end)
+    |> Enum.reduce(0, fn valve_to_open, acc ->
+      travel_time = shortest_path_length(valves, current_valve, valve_to_open, memo)
+      time_to_open_valve = 1
+      new_minutes_elapsed = minutes_elapsed + travel_time + time_to_open_valve
+
+      max(
+        open_valves_helper(
+          valves,
+          valves_to_open,
+          time_limit,
+          new_minutes_elapsed,
+          [valve_to_open | path],
+          Map.put(open_valves, valve_to_open, new_minutes_elapsed),
+          memo
+        ),
+        acc
+      )
+    end)
+  end
+
+  defp open_valves(valves, valves_to_open, start, time_limit, memo) do
+    open_valves_helper(valves, valves_to_open, time_limit, 0, [start], %{}, memo)
+  end
+
+  defp print_helper(
+         valves,
+         current,
+         flags,
+         visited,
+         is_last,
+         depth
+       ) do
+    %Valve{name: name, flow_rate: flow_rate, neighbors: neighbors} = Map.get(valves, current)
+
+    if MapSet.member?(visited, current) do
+      nil
+    else
+      cond do
+        depth == 0 ->
+          IO.puts("(#{name}|#{flow_rate})")
+
+        true ->
+          Enum.each(1..depth, fn d ->
+            if not MapSet.member?(flags, d) do
+              IO.write("    ")
+            else
+              IO.write("|    ")
+            end
+          end)
+
+          if is_last do
+            IO.puts("+--- (#{name}|#{flow_rate})")
+            MapSet.put(flags, depth)
+          else
+            IO.puts("+--- (#{name}|#{flow_rate})")
+          end
+      end
+
+      new_visited = MapSet.put(visited, current)
+
+      Enum.with_index(neighbors)
+      |> Enum.each(fn {n, i} ->
+        print_helper(valves, n, flags, new_visited, length(neighbors) - 1 == i, depth + 1)
+      end)
+    end
+  end
+
+  def print(tree) do
+    print_helper(tree, "AA", MapSet.new(), MapSet.new(), false, 0)
+  end
+
+  def part1(input) do
+    {:ok, memo} = Agent.start_link(fn -> %{} end, name: __MODULE__)
+
+    valves = parse(input)
+
+    valves_to_open =
+      Enum.filter(valves, &should_open_valve?/1)
+      |> Enum.map(fn {k, _} -> k end)
+
+    open_valves(valves, valves_to_open, "AA", 30, memo)
   end
 
   def part2(_args) do
--- test/advent_of_code/day_16_test.exs
+++ test/advent_of_code/day_16_test.exs
@@ -3,12 +3,108 @@ defmodule AdventOfCode.Day16Test do
 
   import AdventOfCode.Day16
 
-  @tag :skip
+  @input """
+  Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+  Valve BB has flow rate=13; tunnels lead to valves CC, AA
+  Valve CC has flow rate=2; tunnels lead to valves DD, BB
+  Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+  Valve EE has flow rate=3; tunnels lead to valves FF, DD
+  Valve FF has flow rate=0; tunnels lead to valves EE, GG
+  Valve GG has flow rate=0; tunnels lead to valves FF, HH
+  Valve HH has flow rate=22; tunnel leads to valve GG
+  Valve II has flow rate=0; tunnels lead to valves AA, JJ
+  Valve JJ has flow rate=21; tunnel leads to valve II
+  """
+  test "parse input" do
+    result = parse(@input)
+
+    assert result == %{
+             "AA" => %AdventOfCode.Day16.Valve{
+               name: "AA",
+               flow_rate: "0",
+               neighbors: ["DD", "II", "BB"]
+             },
+             "BB" => %AdventOfCode.Day16.Valve{
+               name: "BB",
+               flow_rate: "13",
+               neighbors: ["CC", "AA"]
+             },
+             "CC" => %AdventOfCode.Day16.Valve{
+               name: "CC",
+               flow_rate: "2",
+               neighbors: ["DD", "BB"]
+             },
+             "DD" => %AdventOfCode.Day16.Valve{
+               name: "DD",
+               flow_rate: "20",
+               neighbors: ["CC", "AA", "EE"]
+             },
+             "EE" => %AdventOfCode.Day16.Valve{
+               name: "EE",
+               flow_rate: "3",
+               neighbors: ["FF", "DD"]
+             },
+             "FF" => %AdventOfCode.Day16.Valve{
+               name: "FF",
+               flow_rate: "0",
+               neighbors: ["EE", "GG"]
+             },
+             "GG" => %AdventOfCode.Day16.Valve{
+               name: "GG",
+               flow_rate: "0",
+               neighbors: ["FF", "HH"]
+             },
+             "HH" => %AdventOfCode.Day16.Valve{
+               name: "HH",
+               flow_rate: "22",
+               neighbors: ["GG"]
+             },
+             "II" => %AdventOfCode.Day16.Valve{
+               name: "II",
+               flow_rate: "0",
+               neighbors: ["AA", "JJ"]
+             },
+             "JJ" => %AdventOfCode.Day16.Valve{
+               name: "JJ",
+               flow_rate: "21",
+               neighbors: ["II"]
+             }
+           }
+  end
+
+  test "shortest_path_length" do
+    {:ok, memo} = Agent.start_link(fn -> %{} end, name: __MODULE__)
+    valves = parse(@input)
+    result = shortest_path_length(valves, "AA", "HH", memo)
+
+    assert result == 5
+
+    result = shortest_path_length(valves, "AA", "BB", memo)
+
+    assert result == 1
+
+    result = shortest_path_length(valves, "JJ", "HH", memo)
+
+    assert result == 7
+  end
+
   test "part1" do
-    input = nil
+    input = """
+    Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+    Valve BB has flow rate=13; tunnels lead to valves CC, AA
+    Valve CC has flow rate=2; tunnels lead to valves DD, BB
+    Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+    Valve EE has flow rate=3; tunnels lead to valves FF, DD
+    Valve FF has flow rate=0; tunnels lead to valves EE, GG
+    Valve GG has flow rate=0; tunnels lead to valves FF, HH
+    Valve HH has flow rate=22; tunnel leads to valve GG
+    Valve II has flow rate=0; tunnels lead to valves AA, JJ
+    Valve JJ has flow rate=21; tunnel leads to valve II
+    """
+
     result = part1(input)
 
-    assert result
+    assert result == 1645
   end
 
   @tag :skip
